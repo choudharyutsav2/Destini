c864838941280c394fb0a41d44ee25ba57965bab
Utsav Kumar Choudhary <choudharyutsav2@gmail.com>
2024-03-24 14:02:50 +0530

    Add files via upload

---
+++ lib/story.dart
@@ -0,0 +1,12 @@
+class Story{
+  late String storyTitle;
+  late String choice1;
+  late String choice2;
+
+  Story({required String T, required String c1,required String c2}){
+    storyTitle=T;
+    choice1=c1;
+    choice2=c2;
+  }
+}
+
